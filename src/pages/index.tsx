372d9e3aef646fb13e23723a7a4314c4cc0f6c12
Adil Mektoub <adil.mektoub@caast.tv>
2025-03-12 07:15:55 +0100

    put in place favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,9 @@ export default function Home() {
         <meta name="description" content="Expert DevOps/SRE spécialisé dans le déploiement haute disponibilité de la plateforme Open e-Mobility pour un avenir plus vert et durable." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       
       <Header />
